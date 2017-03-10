887e736ec922156f0884577baf07eef6945fd803
grapecity-korea <support-kor@grapecity.com>
2017-03-10 16:24:55 +0900

    scrollbar 컴포넌트 추가-Paul
    scrollbar 컴포넌트 추가-Paul

--- PureJS-LearnSpreadJS/src/app/app-routing.module.ts
+++ PureJS-LearnSpreadJS/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { InstallationComponent } from "./installation/installation.component";
 import { RequirementsComponent } from "./requirements/requirements.component";
 import { LicensingComponent } from "./licensing/licensing.component";
 import { InitializeComponent } from "./core/initialize/initialize.component";
-
+import { ScrollBarComponent } from './core/Spread/scroll-bar/scroll-bar.component';
 
 
 const APP_ROUTES:Routes = [
@@ -16,7 +16,8 @@ const APP_ROUTES:Routes = [
     { path: 'installation', component: InstallationComponent },      
     { path: 'requirements', component: RequirementsComponent },      
     { path: 'licensing', component: LicensingComponent },              
-    { path: 'initialize', component: InitializeComponent },      
+    { path: 'initialize', component: InitializeComponent },     
+    { path: 'ScrollBar', component: ScrollBarComponent },  
 ];
 
 @NgModule({
