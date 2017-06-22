090698771f42bf286f92407005f850240521b733
grapecity-korea <support-kor@grapecity.com>
2017-06-22 15:50:14 +0900

    0622 Nancy
    0622 Nancy

--- PureJS-LearnSpreadJS/src/app/overview/overview.component.ts
+++ PureJS-LearnSpreadJS/src/app/overview/overview.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router, RouterModule} from '@angular/router';
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewComponent implements OnInit {
 
-  constructor() { }
+   constructor(private router: Router) { }
+
+   onSelect(url: string) {
+      console.log(url);
+    this.router.navigate([url]);
+  }
 
   ngOnInit() {
   }
